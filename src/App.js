123567df922b48fc64ec4f048a179e7d2c3dbe18
Pallavi Langhe <91686147+Pallavi-Langhe@users.noreply.github.com>
2022-12-06 01:10:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+import BooksInfo from "./Books";
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("business");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1> 📚 goodbooks </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(BooksInfo).map((genre) => (
+          <button onClick={() => genreClickHandler(genre)}>{genre}</button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {BooksInfo[selectedGenre].map((book) => (
+            <li key={book.name}>
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/Books.js
@@ -0,0 +1,80 @@
+const BooksInfo = {
+  javascript: [
+    { name: "Eloquent JavaScript", rating: "4/5" },
+    { name: "You Don't Know JS", rating: "3.5/5" },
+    { name: "A Smarter Way to Learn JavaScript", rating: "4.5/5" }
+  ],
+
+  fiction: [
+    {
+      name: "Shiva Trilogy",
+      rating: "5/5"
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      rating: "4.5/5"
+    },
+    {
+      name: "One Hundred Years of Solitude",
+      rating: "4/5"
+    }
+  ],
+  business: [
+    {
+      name: "Never Split the Difference",
+      rating: "3.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
+    },
+    {
+      name: "Zero to One",
+      rating: "5/5"
+    }
+  ],
+  Music: [
+    {
+      name: "Industry Baby",
+      rating: "4.5/5"
+    },
+    {
+      name: "Loco contigo",
+      rating: "4/5"
+    },
+    {
+      name: "Alone",
+      rating: "4/5"
+    }
+  ],
+  Food: [
+    {
+      name: "Vada Pav",
+      rating: "5/5"
+    },
+    {
+      name: "Pani Puri",
+      rating: "3.5/5"
+    },
+    {
+      name: "Pizza",
+      rating: "3/5"
+    }
+  ],
+  Travel: [
+    {
+      name: "Dubai",
+      rating: "4.5/5"
+    },
+    {
+      name: "India",
+      rating: "5/5"
+    },
+    {
+      name: "maldives",
+      rating: "5/5"
+    }
+  ]
+};
+
+export default BooksInfo;
